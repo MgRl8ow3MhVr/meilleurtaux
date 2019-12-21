d0872e8a18faefba4c7b93a8f4a47e4e9e3065ee
MgRl8ow3MhVr <7anUDQ2A3MVJkC7J@gmail.com>
2019-12-21 18:10:37 +0100

    changement adresse back

--- src/components/BackEndAddress.js
+++ src/components/BackEndAddress.js
@@ -1,3 +1,4 @@
-const BackEndAdress = "http://localhost:3100";
+// const BackEndAdress = "http://localhost:3100";
+const BackEndAdress = "https://backendmeilleurtaux.herokuapp.com";
 
 export default BackEndAdress;
