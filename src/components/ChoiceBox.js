7dc243c379be196d868f2b1fec382104e2e9a202
MgRl8ow3MhVr <7anUDQ2A3MVJkC7J@gmail.com>
2019-12-21 21:20:03 +0100

    style ajustement et radiobuttons svg

--- src/components/ChoiceBox.js
+++ src/components/ChoiceBox.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import radio_button_checked from "../assets/radio_button_checked.svg";
+import radio_button_unchecked from "../assets/radio_button_unchecked.svg";
 
 // CHOICE BOXES FROM FIRST 4 PAGES
 
@@ -10,7 +12,8 @@ const ChoiceBox = ({ name, chosen, checked, next }) => {
       onClick={chosen}
       className={checked ? "choicebox selected" : "choicebox"}
     >
-      <input type="radio" checked={checked} readOnly />
+      {/* <input type="radio" checked={checked} readOnly /> */}
+      <img src={checked ?radio_button_checked:radio_button_unchecked} />
       <div style={{ color: checked && "white" }}>{name.toUpperCase()}</div>
     </Link>
   );
